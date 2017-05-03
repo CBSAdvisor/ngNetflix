8b72e7463e423d65d429773b83c3f896d27580d1
Dmitry Shvaikovski <d.shvaikovski@technikon.by>
2017-05-03 17:22:54 +0300

    Add angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,5 @@ export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'fog', loadChildren: './+fog/fog.module#FoGModule' },
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
   { path: '**', component: NoContentComponent },
 ];
