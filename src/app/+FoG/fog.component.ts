c2b616609e188c631b86a3722532eb766582d5f1
CBSAdvisor <cbs.advisor@gmail.com>
2017-05-12 18:02:57 +0300

    Get collection by service

--- src/app/+FoG/fog.component.ts
+++ src/app/+FoG/fog.component.ts
@@ -2,20 +2,27 @@ import {
   Component,
   OnInit
 } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { FoGService } from './fog.service';
+import { FoGHero } from './FoG.model';
 
 @Component({
   selector: 'fog',
   templateUrl: './fog.component.html',
-  styleUrls: ['./fog.component.scss']
+  styleUrls: ['./fog.component.scss'],
+  providers: [FoGService]
 })
 export class FoGComponent implements OnInit {
+  public heroes$: Observable<FoGHero[]>;
 
-  constructor() {
+  constructor(private fogService: FoGService) {
     console.log('CONSTRUCTOR FoGComponent');
+    this.heroes$ = this.fogService.heroesCollection;
   }
 
   public ngOnInit() {
     console.log('OmInit FoGComponent');
+    this.fogService.loadCollection();
   }
 
 }
