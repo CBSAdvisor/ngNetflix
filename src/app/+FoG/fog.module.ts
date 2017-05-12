65da089bd40ca7c0bb7c481b4db1cf6994c9b24a
CBSAdvisor <cbs.advisor@gmail.com>
2017-05-12 18:02:15 +0300

    Add FoGService

--- src/app/+FoG/fog.module.ts
+++ src/app/+FoG/fog.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { routes } from './fog.routes';
-import { FoGComponent } from './fog.component';
+import { HttpModule, JsonpModule } from '@angular/http';
 import { MdButtonModule, MdCardModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { routes } from './fog.routes';
+import { FoGComponent } from './fog.component';
+import { FoGService } from './fog.service';
 
 @NgModule({
   declarations: [
     FoGComponent,
   ],
   imports: [
+    CommonModule,
+    HttpModule,
+    JsonpModule,
     FlexLayoutModule,
     MdCardModule,
     MdButtonModule,
     RouterModule.forChild(routes)
   ],
+  providers: [
+    FoGService
+  ]
 })
 export class FoGModule {
   public static routes = routes;
