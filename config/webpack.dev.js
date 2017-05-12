9e2bad94146c4d95deb49736c3e81cc3f7b76a35
Dmitry Shvaikovski <d.shvaikovski@technikon.by>
2017-05-12 08:21:57 +0300

    Change avatar size

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -220,6 +220,7 @@ module.exports = function (options) {
     devServer: {
       port: METADATA.port,
       host: METADATA.host,
+      disableHostCheck: true,
       historyApiFallback: true,
       watchOptions: {
         aggregateTimeout: 300,
